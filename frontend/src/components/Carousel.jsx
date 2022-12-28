b347da54ddedd4efed097e1e74714042babfb2e6
ricardorojass <reddrum7@gmail.com>
2022-12-28 18:31:44 -0500

    remove css code

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -1,51 +1,9 @@
-import Carousel from 'react-bootstrap/Carousel';
-import {jsx} from '@emotion/react'
-import { css } from '@emotion/css'
 import * as React from 'react'
 
 function MainCarousel() {
   return (
-    <div className='carouselContainer'>
-      <Carousel fade>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/500"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/500"
-            alt="Second slide"
-          />
+    <div>
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/500"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      <div>Hola</div>
     </div>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,50 +1,35 @@
-/** @jsx jsx */
-import {jsx} from '@emotion/react'
-import { css } from '@emotion/css'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { Cart3, Search } from 'react-bootstrap-icons'
 import { Button } from './lib'
 import * as colors from '../styles/colors'
 
-const LinkStyle = css({
-  color: colors.purple20,
-  fontSize: '1.1rem'
-})
-
+{/* <header css={{
+  padding: '10px 20px',
+  display: 'flex',
+  backgroundColor: colors.yellow,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
+  borderBottom: `0.5px solid ${colors.purple}`
+}}> */}
 function Header({title}) {
 
   return (
     <>
-      <header css={{
-        padding: '10px 20px',
-        display: 'flex',
-        backgroundColor: colors.yellow,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        width: '100%',
-        borderBottom: `0.5px solid ${colors.purple}`
-      }}>
-        <h1 css={{color: colors.purple20}}>{title}</h1>
+        <h1>{title}</h1>
         <div>
           <Button variant='secondary' onClick={() => alert('search clicked')}>
             <Search  size='25'/>
           </Button>
           <Button variant='secondary' onClick={() => alert('cart clicked')}>
-            <Cart3  size='25'/>
+            {/* <Cart3  size='25'/> */}
           </Button>
         </div>
       </header>
-      <div css={{
-        padding: '5px 20px',
-        display: 'flex',
-        gap: '1rem',
-        alignItems: 'center',
-        borderBottom: `0.5px solid ${colors.purple}`
-      }}>
-        <Link className={LinkStyle} to="/tienda">Tienda</Link>
-        <Link className={LinkStyle} to="/contacto">Contacto</Link>
+      <div>
+        <Link to="/tienda">Tienda</Link>
+        <Link to="/contacto">Contacto</Link>
 
       </div>
     </>
