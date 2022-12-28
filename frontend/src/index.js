fd93b4d944730d2e83e9e40b3a85da9545f24c3f
ricardorojass <reddrum7@gmail.com>
2022-12-27 22:45:19 -0500

    add emotion ui lib

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
-import * as React from 'react';
-import App from "./App";
+import * as React from 'react'
+import './bootstrap'
+import App from "./App"
 
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App/>);
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App/>)
