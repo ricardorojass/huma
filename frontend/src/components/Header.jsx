3d69fe1907b1041982fb67372c6ab2993cfe4c92
ricardorojass <reddrum7@gmail.com>
2022-12-28 02:24:01 -0500

    add styles to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,56 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/react'
+import { css } from '@emotion/css'
 import * as React from 'react'
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+import { Cart3, Search } from 'react-bootstrap-icons'
+import { Button } from './lib'
+import * as colors from '../styles/colors'
 
-class Header extends React.Component {
+const LinkStyle = css({
+  color: colors.purple20,
+  fontSize: '1.1rem'
+})
 
-  render() {
-    return (
-      <Container fluid className='header'>
-        <Row>
-          <Col>
-            <span>{this.props.title}</span>
-          </Col>
-          <Col>
-            <Button variant="primary" onClick={() => alert('login clicked')}>Login</Button>
-            <Button variant="secondary" onClick={() => alert('register clicked')}>Register</Button>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+
+
+function Header({title}) {
+
+  return (
+    <>
+      <header css={{
+        padding: '10px 20px',
+        display: 'flex',
+        backgroundColor: colors.yellow,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+        borderBottom: '0.5px solid'
+      }}>
+        <h1 css={{color: colors.purple20}}>{title}</h1>
+        <div>
+          <Button variant='secondary' onClick={() => alert('search clicked')}>
+            <Search  size='25'/>
+          </Button>
+          <Button variant='secondary' onClick={() => alert('cart clicked')}>
+            <Cart3  size='25'/>
+          </Button>
+        </div>
+      </header>
+      <div css={{
+        padding: '5px 20px',
+        display: 'flex',
+        gap: '1rem',
+        alignItems: 'center',
+        borderBottom: '0.5px solid'
+      }}>
+        <Link className={LinkStyle} to="/tienda">Tienda</Link>
+        <Link className={LinkStyle} to="/contacto">Contacto</Link>
+
+      </div>
+    </>
+  )
 }
 
 Header.defaultProps = {
---
+++ frontend/src/components/lib.js
@@ -0,0 +1,31 @@
+
+/** @jsx jsx */
+import {jsx, keyframes} from '@emotion/react'
+import styled from '@emotion/styled'
+import * as colors from '../styles/colors'
+
+const spin = keyframes({
+  '0%': {transform: 'rotate(0deg)'},
+  '100%': {transform: 'rotate(360deg)'},
+})
+
+const buttonVariants = {
+  primary: {
+    background: colors.indigo,
+    color: colors.base,
+  },
+  secondary: {
+    background: colors.gray,
+    color: colors.text,
+  },
+}
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+  },
+  ({variant = 'primary'}) => buttonVariants[variant],
+)
+
+export {Button}
